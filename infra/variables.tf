d4b2b2e401b9b45dfb16d840da3342601501b2ed
m.kharrat <m.kharrat@signup-design.de>
2024-01-30 23:09:30 +0100

    first commit test React

---
+++ infra/variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+  type    = string
+  default = "eu-central-1"
+}
+variable "aws_profile" {
+  type    = string
+  default = "153386709997_Student"
+}
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1>First React Website by Marouan Kh</h1>
+  );
+}
+
+export default App;
